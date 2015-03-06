fb0696f6bd9ec0280d56f9ad0840d871fac2e2c9
gyufa <gyulavoros87@gmail.com>
2015-03-06 10:23:17 +0100

    MathUtils

--- core/src/main/java/com/bootstrap/utils/MathUtils.java
+++ core/src/main/java/com/bootstrap/utils/MathUtils.java
@@ -9,4 +9,10 @@ public final class MathUtils {
     if (value > max) return max;
     return value;
   }
+
+  public static float clamp(final float value, final float min, final float max) {
+    if (value < min) return min;
+    if (value > max) return max;
+    return value;
+  }
 }
