66bbd1fb03259e5137918474f7c3634800fbe74d
Andras Kindler <andraskindler@gmail.com>
2015-03-16 13:43:42 +0100

    icon, intro and theme changes

--- core/src/main/java/com/bootstrap/utils/MaterialColors.java
+++ core/src/main/java/com/bootstrap/utils/MaterialColors.java
@@ -1,23 +1,23 @@
 package com.bootstrap.utils;
 
 public interface MaterialColors {
-  public static final int RED = 0xFFF44336;
-  public static final int PINK = 0xFFE91E63;
-  public static final int PURPLE = 0xFF9C27B0;
-  public static final int DEEP_PURPLE = 0xFF673AB7;
-  public static final int INDIGO = 0xFF3F51B5;
-  public static final int BLUE = 0xFF2196F3;
-  public static final int LIGHT_BLUE = 0xFF03A9F4;
-  public static final int CYAN = 0xFF00BCD4;
-  public static final int TEAL = 0xFF009688;
-  public static final int GREEN = 0xFF4CAF50;
-  public static final int LIGHT_GREEN = 0xFF8BC34A;
-  public static final int LIME = 0xFFCDDC39;
-  public static final int YELLOW = 0xFFFFEB3B;
-  public static final int AMBER = 0xFFFFC107;
-  public static final int ORANGE = 0xFFFF9800;
-  public static final int DEEP_ORANGE = 0xFFFF5722;
-  public static final int BROWN = 0xFF795548;
-  public static final int GREY = 0xFF9E9E9E;
-  public static final int BLUE_GREY = 0xFF607D8B;
+  int RED = 0xFFF44336;
+  int PINK = 0xFFE91E63;
+  int PURPLE = 0xFF9C27B0;
+  int DEEP_PURPLE = 0xFF673AB7;
+  int INDIGO = 0xFF3F51B5;
+  int BLUE = 0xFF2196F3;
+  int LIGHT_BLUE = 0xFF03A9F4;
+  int CYAN = 0xFF00BCD4;
+  int TEAL = 0xFF009688;
+  int GREEN = 0xFF4CAF50;
+  int LIGHT_GREEN = 0xFF8BC34A;
+  int LIME = 0xFFCDDC39;
+  int YELLOW = 0xFFFFEB3B;
+  int AMBER = 0xFFFFC107;
+  int ORANGE = 0xFFFF9800;
+  int DEEP_ORANGE = 0xFFFF5722;
+  int BROWN = 0xFF795548;
+  int GREY = 0xFF9E9E9E;
+  int BLUE_GREY = 0xFF607D8B;
 }
