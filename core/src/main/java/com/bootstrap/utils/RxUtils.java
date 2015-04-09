e39adb93c224d074e6ad16bbbd196d5398710ad7
Andras Kindler <andraskindler@gmail.com>
2015-04-09 15:23:45 +0200

    SafeUnsubscribe now accepts multiple Subscriptions

--- core/src/main/java/com/bootstrap/utils/RxUtils.java
+++ core/src/main/java/com/bootstrap/utils/RxUtils.java
@@ -6,9 +6,11 @@ public final class RxUtils {
   private RxUtils() {
   }
 
-  public static void safeUnsubscribe(final Subscription subscription) {
-    if (subscription != null) {
-      subscription.unsubscribe();
+  public static void safeUnsubscribe(final Subscription... subscriptions) {
+    for (Subscription subscription : subscriptions) {
+      if (subscription != null) {
+        subscription.unsubscribe();
+      }
     }
   }
 }
