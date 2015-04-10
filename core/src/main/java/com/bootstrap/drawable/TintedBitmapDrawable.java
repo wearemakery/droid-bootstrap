92123432fa70ccf46d97a36366f2e861ef3408e7
gyufa <gyulavoros87@gmail.com>
2015-04-10 14:19:35 +0200

    TintedBitmapDrawable alpha support

--- core/src/main/java/com/bootstrap/drawable/TintedBitmapDrawable.java
+++ core/src/main/java/com/bootstrap/drawable/TintedBitmapDrawable.java
@@ -4,11 +4,14 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.LightingColorFilter;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 
 public final class TintedBitmapDrawable extends BitmapDrawable {
   private int tint;
+  private int alpha;
 
   public TintedBitmapDrawable(final Resources res, final Bitmap bitmap, final int tint) {
     super(res, bitmap);
@@ -18,15 +21,19 @@ public final class TintedBitmapDrawable extends BitmapDrawable {
   public TintedBitmapDrawable(final Resources res, final int resId, final int tint) {
     super(res, BitmapFactory.decodeResource(res, resId));
     this.tint = tint;
+    this.alpha = Color.alpha(tint);
   }
 
   public void setTint(final int tint) {
     this.tint = tint;
+    this.alpha = Color.alpha(tint);
   }
 
   @Override public void draw(final Canvas canvas) {
-    if (getPaint().getColorFilter() == null) {
-      getPaint().setColorFilter(new LightingColorFilter(tint, 0));
+    final Paint paint = getPaint();
+    if (paint.getColorFilter() == null) {
+      paint.setColorFilter(new LightingColorFilter(tint, 0));
+      paint.setAlpha(alpha);
     }
     super.draw(canvas);
   }
